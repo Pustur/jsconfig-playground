be1dbc3b983cf621ca8aa8764b9fc32de0ea4711
Loris Bettazza <loris.bettazza@gmail.com>
2022-10-24 16:56:21 +0200

    3. Add click event on box

--- Box01.js
+++ Box01.js
@@ -1,5 +1,9 @@
 class Box01 {
   constructor() {
     console.log('window width', Utils.getWindowWidth());
+
+    const box = document.querySelector('.box');
+
+    box.addEventListener('click', console.log);
   }
 }
